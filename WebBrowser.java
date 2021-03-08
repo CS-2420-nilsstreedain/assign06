8a9d5f74dd273f2cca90cdedb7ed41579bf78166
Nils Streedain <business@nilsstreedain.com>
2021-03-08 16:06:51 -0700

    Initial Commit

---
+++ WebBrowser.java
@@ -0,0 +1,29 @@
+package assign06;
+
+import java.util.NoSuchElementException;
+
+public class WebBrowser {
+	public WebBrowser() {
+		
+	}
+	
+	public WebBrowser(SinglyLinkedList<URL> history) {
+		
+	}
+	
+	public void visit(URL webpage) {
+		
+	}
+	
+	public URL back() throws NoSuchElementException {
+		
+	}
+	
+	public URL forward() throws NoSuchElementException {
+		
+	}
+	
+	public SinglyLinkedList<URL> history() {
+		
+	}
+}
