9e1d62c1a5297ea52d937c6bc388f083b2b50b10
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2021-03-11 01:45:11 -0700

    Update StackComparisonTimer.java

--- StackComparisonTimer.java
+++ StackComparisonTimer.java
@@ -1,7 +1,7 @@
 package assign06;
 
 /**
- * This class collects running times for methods of LinkedListStack and ArrayStack.
+ * This class collects running times for methods of ArrayListSorter.
  * 
  * @author Erin Parker, Paul Nuffer, Nils Streedain
  * @version February 9, 2021
@@ -14,19 +14,56 @@ public class StackComparisonTimer {
 		int incr = 100000000;
 		for(int probSize = 100000000; probSize <= 2000000000; probSize += incr) {
 			
-			int timesToLoop = 100000000;
-
-			long stopTime, midpointTime, startTime = System.nanoTime();
+			int timesToLoop = 10000000;
 
 			// First, spin computing stuff until one second has gone by.
 			// This allows this thread to stabilize.
-			while(System.nanoTime() - startTime < Integer.MAX_VALUE) {}
 
+			long stopTime, midpointTime, startTime = System.nanoTime();
+
+			while(System.nanoTime() - startTime < Integer.MAX_VALUE) {}
+			
+			//ListStack Pop Setup
+		//	LinkedListStack<Integer> listStack = new LinkedListStack<>();
+		//	for (int i = 0; i < probSize; i++) 
+		//		listStack.push(0);
+			
+			//ArrayStack Pop Setup
+		//	ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		//	for (int i = 0; i < probSize; i++) 
+		//		arrayStack.push(0);
+			
+			//ListStack Peek Setup
+		//	LinkedListStack<Integer> listStack = new LinkedListStack<>();
+		//	for (int i = 0; i < probSize; i++) 
+		//		listStack.push(0);
+			
+			//ArrayStack Peek Setup
+		//	ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		//	for (int i = 0; i < probSize; i++) 
+		//		arrayStack.push(0);
+			
 			// Collect running times.
 			startTime = System.nanoTime();
 			for(int i = 0; i < timesToLoop; i++) {
-				ArrayStack<Integer> stack = new ArrayStack<>();
-				stack.push(i);
+				LinkedListStack<Integer> listStack = new LinkedListStack<>();
+				for (int j = 0; j < 3; j++)
+					listStack.push(i);
+				
+		//		ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		//		for (int j = 0; j < 3; j++)
+		//			arrayStack.push(i);
+				
+		//		listStack.pop();
+				
+		//		arrayStack.pop();	
+						 
+		//		listStack.peek();
+				
+		//		ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		//		arrayStack.push(i);
+		//		for (int j = 0; j < 10000000; j++) 
+		//			arrayStack.peek();
 			}
 
 			midpointTime = System.nanoTime();
@@ -34,8 +71,23 @@ public class StackComparisonTimer {
 			// Capture the cost of running the loop and any other operations done
 			// above that are not the essential method call being timed.
 			for(int i = 0; i < timesToLoop; i++) {
-				@SuppressWarnings("unused")
-				ArrayStack<Integer> stack = new ArrayStack<>();
+		//		LinkedListStack<Integer> listStack = new LinkedListStack<>();
+		//		for (int j = 0; j < 3; j++) {}
+				
+		//		ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		//		for (int j = 0; j < 3; j++) {}
+
+		//		LinkedList Pop no subtract
+				
+		//		ArrayStack Pop no subtract			
+				
+		//		ListStack Peek no subtract
+				
+		//		ArrayStack Peek no subtract		
+				
+		//		ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		//		arrayStack.push(i);
+				
 			}
 
 			stopTime = System.nanoTime();
@@ -43,8 +95,8 @@ public class StackComparisonTimer {
 			// Compute the time, subtract the cost of running the loop
 			// from the cost of running the loop and searching.
 			// Average it over the number of runs.
-			double averageTime = ((midpointTime - startTime) - (stopTime - midpointTime)) / (double) timesToLoop;
-			
+			double averageTime = ((midpointTime - startTime) - 
+						(stopTime - midpointTime)) / (double) timesToLoop;
 			System.out.println(probSize + "\t" + averageTime);
 		}
 	}
